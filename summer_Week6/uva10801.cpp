84371f32b784cdec4c671644ce7781ac4e3ee603
yuchen0515 <i98565412@gmail.com>
2020-09-03 01:10:33 +0800

    summer week6 pE, using dijkstra 最短路 測資換電梯有時會錯誤

--- summer_Week6/uva10801.cpp
+++ summer_Week6/uva10801.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 typedef struct S_pair{
+    int32_t from;
     int32_t to;
     int32_t t;
     int32_t w;
@@ -26,40 +27,67 @@ int main(){
         for (int32_t i = 0 ; i < n ; i++)
             cin >> time_arr[i];
 
-        vector<s_pair> num[k];
-        int32_t short_path[k];
-        int32_t vis[k];
+        vector<s_pair> num[105];
+        int32_t short_path[105];
+        //int32_t vis[k];
 
-        for (int32_t i = 0 ; i < k; i++){
+        for (int32_t i = 0 ; i < 105; i++){
             short_path[i] = 1000000;
-            vis[i] = 0;
         }
         
+
         priority_queue<s_pair, vector<s_pair>, cmp> stl;
 
         stl.push({0, 0, 0});
         short_path[0] = 0;
+        
+        char str[100], tp_str;
+        int32_t tp_x = 0;
+        cin.getline(str, 100, '\n');
 
-        //思考電梯存法，更新方法
+        for (int32_t i = 0 ; i < n ; i ++){
+            vector<int32_t> arr;
+            while (1){
+                scanf("%d%c", &tp_x, &tp_str);
+                arr.push_back(tp_x);
+                if (tp_str == '\n')
+                    break;
+            }
 
-        /*
+            for (int32_t j = 0 ; j < arr.size(); j++){
+                for (int32_t k = 0 ; k < arr.size() ; k++){
+                    if (j==k) continue;
+                    num[arr[j]].push_back({arr[j], arr[k], i, abs(arr[j]-arr[k]) * time_arr[i]});
+                }
+            }
+        }
+
+        if (num[0].size() != 0)
+            stl.push(num[0][0]);
+
         while (!stl.empty()){
             s_pair temp = stl.top();
             stl.pop();
 
-            for (auto c: num[temp.sta]){
-                if (short_path[c.sta] + c.w < short_path[c.end]){
-                    short_path[c.end] = c.w + short_path[c.sta];
-                    stl.push({c.end, c.sta, c.w});
+            for (auto c: num[temp.from]){
+                int32_t cmpp = short_path[c.from] + c.w;
+                
+                //有問題
+                if (temp.t != c.t){
+                    cmpp += 60;
+                }
+
+                if (cmpp < short_path[c.to]){
+                    short_path[c.to] = cmpp;
+                    stl.push({c.to, c.from, c.t, c.w});
                 }
             }
         }
-        */
 
-        if (short_path[k-1] == 1000000)
+        if (short_path[k] == 1000000)
             cout << "IMPOSSIBLE" << endl;
         else 
-            cout << short_path[k-1] << endl;
+            cout << short_path[k] << endl;
     }
 
 
